4b1edc4e90311c3d4e58c78f2568b63709c49e3c
vokelilekov <vokelilekov@gmail.com>
2024-03-28 17:31:53 +0300

    'first-commit'

---
+++ Lab2/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    url = request.url
+    title = "Лр 2"
+    return render_template("index.html", title=title, url=url)
+
+@app.route('/request_data')
+def request_data():
+    return render_template('request_data.html',
+                           url_params=request.args,
+                           headers=request.headers,
+                           cookies=request.cookies,
+                           form_data=request.form)
+
+@app.route('/phone', methods=['GET', 'POST'])
+def phone():
+    error = None
+    formatted_number = None
+    if request.method == 'POST':
+        phone_number = request.form.get('phone_number', '')
+        error = validate_phone_number(phone_number)
+        if not error:
+            formatted_number = format_phone_number(phone_number)
+    return render_template('phone_form.html', error=error, formatted_number=formatted_number)
+
+def validate_phone_number(phone_number):
+    cleaned_number = ''.join(filter(str.isdigit, phone_number))
+    if not cleaned_number:
+        return "Введите номер телефона"
+    if len(cleaned_number) == 10:
+        return None
+    if len(cleaned_number) == 11 and cleaned_number.startswith(('7', '8')):
+        return None
+    return "Недопустимый ввод. Неверное количество цифр или некорректный формат номера."
+
+def format_phone_number(phone_number):
+    cleaned_number = ''.join(filter(str.isdigit, phone_number))
+    if len(cleaned_number) == 11 and cleaned_number.startswith('8'):
+        cleaned_number = '7' + cleaned_number[1:]
+    
+    if len(cleaned_number) == 10:
+        formatted_number = f'7-{cleaned_number[:3]}-{cleaned_number[3:6]}-{cleaned_number[6:8]}-{cleaned_number[8:]}'
+    elif len(cleaned_number) == 11:
+        formatted_number = f'{cleaned_number[:1]}-{cleaned_number[1:4]}-{cleaned_number[4:7]}-{cleaned_number[7:9]}-{cleaned_number[9:]}'
+    else:
+        return "Недопустимый ввод. Неверное количество цифр или некорректный формат номера."
+    
+    return formatted_number
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
