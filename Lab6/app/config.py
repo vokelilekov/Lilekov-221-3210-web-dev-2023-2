6fea548c1cd9c22975fbb2fd53ba5bb68a866b2d
Вадим Лилеков <vokelilekov@gmail.com>
2024-06-01 16:21:25 +0300

    'lab6'

---
+++ Lab6/app/config.py
@@ -0,0 +1,10 @@
+import os
+
+SECRET_KEY = 'secret-key'
+
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///project.db'
+SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://std_2472_6:TaySwiFan1989.dat@std-mysql.ist.mospolytech.ru/std_2472_6'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = True
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media', 'images')
